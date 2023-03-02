1f33974e1544f4e5c7360c4c0e3735447395b799
thungcodejs <ptuanhungg@gmail.com>
2023-03-02 11:05:04 +0700

    storing found users in the state

--- store/Store.js
+++ store/Store.js
@@ -1,1 +1,1 @@
-import { configureStore } from "@reduxjs/toolkit";import authSlice from "./authSlice";export const store = configureStore({    reducer: {        auth: authSlice    }})
+import { configureStore } from "@reduxjs/toolkit";import authSlice from "./authSlice";import UserSlice from "./UserSlice";export const store = configureStore({    reducer: {        auth: authSlice,        users: UserSlice    }})
